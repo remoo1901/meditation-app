aa1c8c1739f0425aa5c6af523def2883ea47abdf
Remon Abdou <remoo1901@hotmail.com>
2021-03-10 22:29:57 -0500

    start working on App.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const app  = ( )=> {
+        const song = document.querySelector('.song');
+        const play = document.querySelector('.play');
+        const outline = document.querySelector('.moving-outline circle');
+        const video = document.querySelector('.vid-container video');
+
+        //sound
+
+        const sounds = document.querySelectorAll(''.sound-picker button');
+
+        //Time display
+
+        const timeDisplay = document.querySelector('.time-display');
+
+    }
+
+
+app();
