f0980ebd09af1e56fb09051c37b99ed4b82c8c35
Remon Abdou <remoo1901@hotmail.com>
2021-03-11 21:37:06 -0500

    Working on animated the circle

--- app.js
+++ app.js
@@ -1,18 +1,61 @@
-const app  = ( )=> {
-        const song = document.querySelector('.song');
-        const play = document.querySelector('.play');
-        const outline = document.querySelector('.moving-outline circle');
-        const video = document.querySelector('.vid-container video');
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
 
-        //sound
+  //sound
 
-        const sounds = document.querySelectorAll(''.sound-picker button');
+  const sounds = document.querySelectorAll(".sound-picker button");
 
-        //Time display
+  //Time display
 
-        const timeDisplay = document.querySelector('.time-display');
+  const timeDisplay = document.querySelector(".time-display");
 
+  // Get the length of the outline
+
+  const outlineLength = outline.getTotalLength();
+
+  // Duration
+
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  // Play sound
+
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  // Create a function specific to stop and play sounds
+
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
     }
+  };
 
+  // We can animated the circle
+
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    // Animated the circle
+
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+  };
+};
 
 app();
